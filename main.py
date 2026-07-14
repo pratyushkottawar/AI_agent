1d6edb5d900316c38dbedcc24a74fabfe4c8e0ff
Pratyush Kottawar <pratyushkottawar@gmail.com>
2026-07-14 09:29:04 +0530

    feat(agent): add system prompt

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
-import sys
 from dotenv import load_dotenv
 from google import genai
 import argparse
 from google.genai import types
 from functions.get_file_info import get_files_info
-
+from system_prompt import system_prompt
 
 
 def main():
@@ -20,24 +19,29 @@ def main():
     parser.add_argument("user_prompt", type=str, help="Your help message here")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    
-    
-    messages: list[types.Content] = [
-    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+        
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
     ]
 
-    # response=client.models.generate_content(model="gemini-2.5-flash", contents=messages)
-    response = "Hello response."
-    # if response is None or response.usage_metadata is None:
-    #     print("response is malformed")
-    #     return
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt
+        ),
+    )    
     
-    # print(response.text)
-    # get_files_info(".","./calculator/pkg")
+    # response = "Hello response."
+    if response is None or response.usage_metadata is None:
+        print("response is malformed")
+        return
+    
+    print(response.text)
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
-        # print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        # print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 main()
 
